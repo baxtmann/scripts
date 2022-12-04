d3f8caa51623cc3a925ca795469b8affeeb0f2ae
Brandon Axtmann <baxtmann@hotmail.ca>
2022-12-04 16:59:06 -0500

    adding zabbix agent install script

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Random Scripts I've Made/Used
 # Kubernetes
 - init.sh - Installs latest updates for raspbian and enables virtualization (required step when doing Kubernetes on RPI)
 
+Quick way to pull down script: 
+
+sudo git clone https://github.com/baxtmann/scripts && cd scripts/ && cd monitoring/ && sudo sh ./install-zabbix-agent.sh
---
+++ monitoring/install-zabbix-agent.sh
@@ -0,0 +1,62 @@
+#This script will install Zabbix agent on a Linux server if it does not already exist, and edit the configuration file to point to the Zabbix server.
+
+#Check if Zabbix agent is already installed
+if [ -f /etc/zabbix/zabbix_agentd.conf ]; then
+    echo "Zabbix agent is already installed, we will just update the configuration file."
+    server_hostname=$(hostname)
+    sed -i "s/^Hostname=.*/Hostname=$server_hostname/" /etc/zabbix/zabbix_agentd.conf
+    sed -i 's/^Server=.*/Server=zabbix.jumpstartlabs.co/' /etc/zabbix/zabbix_agentd.conf
+    sed -i "s/^ServerActive=.*/ServerActive=zabbix.jumpstartlabs.co/" /etc/zabbix/zabbix_agentd.conf
+    #now we restart and enable the zabbix agent
+    systemctl restart zabbix-agent
+else
+    #Install Zabbix agent
+    echo "Installing Zabbix agent"
+    #first we need to determine if the os is ubuntu 18 or 20
+    # Check if the lsb_release command is available
+    if command -v lsb_release >/dev/null 2>&1; then
+        # Get the Ubuntu version number
+        ubuntu_version=$(lsb_release -r | awk '{print $2}')
+
+        # Check if the version is 18.04 or 20.04
+        if [[ $ubuntu_version == "18.04" ]]; then
+            # Ubuntu version is 18.04
+            wget https://repo.zabbix.com/zabbix/6.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.2-4%2Bubuntu18.04_all.deb
+            dpkg -i zabbix-release_6.2-4+ubuntu18.04_all.deb
+            apt update
+            apt install zabbix-agent2 zabbix-agent2-plugin-* -y
+            #We need to get the hostname of the server now and add it to the zabbix_agentd.conf file
+            server_hostname=$(hostname)
+            sed -i "s/^Hostname=.*/Hostname=$server_hostname/" /etc/zabbix/zabbix_agentd.conf
+            sed -i 's/^Server=.*/Server=zabbix.jumpstartlabs.co/' /etc/zabbix/zabbix_agentd.conf
+            sed -i "s/^ServerActive=.*/ServerActive=zabbix.jumpstartlabs.co/" /etc/zabbix/zabbix_agentd.conf
+            #now we restart and enable the zabbix agent
+            systemctl restart zabbix-agent2
+            systemctl enable zabbix-agent2
+        fi
+        if [[ $ubuntu_version == "20.04" ]]; then
+            # Ubuntu version is 20.04
+            wget https://repo.zabbix.com/zabbix/6.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.2-4%2Bubuntu20.04_all.deb
+            dpkg -i zabbix-release_6.2-4+ubuntu20.04_all.deb
+            apt update
+            apt install zabbix-agent2 zabbix-agent2-plugin-* -y
+            #We need to get the hostname of the server now and add it to the zabbix_agentd.conf file
+            server_hostname=$(hostname)
+            sed -i "s/^Hostname=.*/Hostname=$server_hostname/" /etc/zabbix/zabbix_agentd.conf
+            sed -i 's/^Server=.*/Server=zabbix.jumpstartlabs.co/' /etc/zabbix/zabbix_agentd.conf
+            sed -i "s/^ServerActive=.*/ServerActive=zabbix.jumpstartlabs.co/" /etc/zabbix/zabbix_agentd.conf
+            #now we restart and enable the zabbix agent
+            systemctl restart zabbix-agent2
+            systemctl enable zabbix-agent2
+        fi
+        else
+            # Ubuntu version is not 18.04 or 20.04, so print an error message and exit
+            echo "Error: This script is only compatible with Ubuntu 18.04 and 20.04."
+            exit 1
+        fi
+    else
+    # lsb_release command is not available, so print an error message and exit
+    echo "Error: lsb_release command not found. Is this a Ubuntu system?"
+    exit 1
+    fi
+fi
